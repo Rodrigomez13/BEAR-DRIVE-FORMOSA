2802d712b07a35c7e512d05a8821014846f3546f
Rodrigomez13 <rodrigonicolasgomez99@gmail.com>
2026-09-11 18:53:21 -0300

    feat(nav): register BearDrive native navigation plugin

--- android/app/src/main/java/com/beardrive/formosa/MainActivity.java
+++ android/app/src/main/java/com/beardrive/formosa/MainActivity.java
@@ -1,5 +1,12 @@
 package com.beardrive.formosa;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(BearDriveNavigationPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
